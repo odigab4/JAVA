afd00f1b46e0893b5de11588f32aec2fde649700
odigab4 <odigab4@gmail.com>
2019-02-18 19:33:41 +0200

    progress 19:33 18/FEB

---
+++ JAVA_D1/src/java_d1/JAVA_D1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_d1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author MahMainBitch
+ */
+public class JAVA_D1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello!");
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int a = 4;
+        a = 6;
+        String simboli = "aa" + "bb";
+        
+        simboli = sc.nextLine();
+        
+        
+        // TODO code application logic here
+    }
+    
+}
